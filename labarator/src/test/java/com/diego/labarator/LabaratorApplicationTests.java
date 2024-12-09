9469af63ccffb9a1a90d7abbd459a6bbf006a33d
DmitriyE <egoroof1@gmail.com>
2024-12-09 17:38:44 +0300

    стартовый Spring

---
+++ labarator/src/test/java/com/diego/labarator/LabaratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.labarator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabaratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
